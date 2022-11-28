336ca77ea694c1713c58bc3a63577bf9b528cd73
naitikvarshney <69815982+naitikvarshney@users.noreply.github.com>
2022-11-28 22:24:32 +0530

    Create Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -15,22 +15,18 @@ train = pd.read_csv('https://raw.githubusercontent.com/dD2405/Twitter_Sentiment_
 # making a copy of dataset
 train_original=train.copy()
 
-train.shape111`         
+train.shape
 
 train_original
 test = pd.read_csv('https://raw.githubusercontent.com/dD2405/Twitter_Sentiment_Analysis/master/test.csv')
 test_original=test.copy()
 test.shape
 test_original
-
+# We combine Train and Test datasets for pre-processing stage
 combine = train.append(test,ignore_index=True,sort=True)
 combine.head()
 combine.tail()
 
-# Removing Twitter Handles (@user)
-# a user-defined function to remove unwanted text patterns from the tweets.
-# It takes two arguments, one is the original string of text and the other is the pattern of text that we want to remove from the string.
-# The function returns the same input string but without the given pattern. 
 
 def remove_pattern(text, pattern):
     # re.findall() finds the pattern i.e @user and puts it in a list for further task
@@ -53,8 +49,13 @@ combine.head(10)
 combine['Tidy_Tweets'] = combine['Tidy_Tweets'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>3]))
 
 combine.head(10)
+
+#Tokenization
+
 tokenized_tweet = combine['Tidy_Tweets'].apply(lambda x: x.split())
 tokenized_tweet.head()
+
+#Stemming
 
 from nltk import PorterStemmer
 
@@ -63,6 +64,8 @@ ps = PorterStemmer()
 tokenized_tweet = tokenized_tweet.apply(lambda x: [ps.stem(i) for i in x])
 
 tokenized_tweet.head()
+
+#Now let’s stitch these tokens back together.
 
 for i in range(len(tokenized_tweet)):
     tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
