f93e14cd4993151affeafed8f3ebd2709518a7dd
naitikvarshney <69815982+naitikvarshney@users.noreply.github.com>
2022-11-28 22:39:38 +0530

    Update Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -77,3 +77,31 @@ for i in range(len(tokenized_tweet)):
 
 combine['Tidy_Tweets'] = tokenized_tweet
 combine.head()
+
+
+
+
+
+
+all_words_negative = ' '.join(text for text in combine['Tidy_Tweets'][combine['label']==1])
+
+# combining the image with the dataset
+Mask = np.array(Image.open(requests.get('http://clipart-library.com/image_gallery2/Twitter-PNG-Image.png', stream=True).raw))
+
+# We use the ImageColorGenerator library from Wordcloud 
+# Here we take the color of the image and impose it over our wordcloud
+image_colors = ImageColorGenerator(Mask)
+
+# Now we use the WordCloud function from the wordcloud library 
+wc = WordCloud(background_color='black', height=1500, width=4000,mask=Mask).generate(all_words_negative)
+
+# Size of the image generated 
+plt.figure(figsize=(10,20))
+
+# Here we recolor the words from the dataset to the image's color
+# recolor just recolors the default colors to the image's blue color
+# interpolation is used to smooth the image generated 
+plt.imshow(wc.recolor(color_func=image_colors),interpolation="gaussian")
+
+plt.axis('off')
+plt.show()
